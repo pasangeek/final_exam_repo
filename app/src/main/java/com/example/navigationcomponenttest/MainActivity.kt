5f3cfd5de40a60616fee96a1fa43a4a92daeb964
pasan wedikkara <pasan.madusanka9@gmail.com>
2023-04-20 22:50:56 +0900

    data binding added partially

--- app/src/main/java/com/example/navigationcomponenttest/MainActivity.kt
+++ app/src/main/java/com/example/navigationcomponenttest/MainActivity.kt
@@ -2,14 +2,24 @@ package com.example.navigationcomponenttest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import com.example.navigationcomponenttest.R.id.nav_host
+import com.example.navigationcomponenttest.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val navController = findNavController(nav_host)
+       binding = DataBindingUtil.setContentView(this ,R.layout.activity_main)
+        binding.lifecycleOwner = this
+
+        navController =  findNavController(nav_host)
 
     }
 
